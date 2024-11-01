18ac948fb81b7c0faf708dd4be27909759d4b479
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-01 18:16:57 +0330

    add FinancialInformation enums

---
+++ src/LoanManagementSystem.Services/FinancialInformation/Enums/IncomeGroup.cs
@@ -0,0 +1,8 @@
+namespace LoanManagementSystem.Services.FinancialInformation.Enums;
+
+public enum IncomeGroup
+{
+    LessThanFive = 0,
+    FiveUptoIncludingTen = 10,
+    MoreThanTen = 20
+}
---
+++ src/LoanManagementSystem.Services/FinancialInformation/Enums/JobType.cs
@@ -0,0 +1,8 @@
+namespace LoanManagementSystem.Services.FinancialInformation.Enums;
+
+public enum JobType
+{
+    Government = 20,
+    SelfEmployed = 10,
+    UnEmployed = 0
+}
