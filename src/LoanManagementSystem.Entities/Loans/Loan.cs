44ab6687cdf0d4948630a4718d1fcfd15e57d7f9
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-02 16:47:08 +0330

    fix Loan properties naming

--- src/LoanManagementSystem.Entities/Loans/Loan.cs
+++ src/LoanManagementSystem.Entities/Loans/Loan.cs
@@ -4,6 +4,6 @@ public class Loan
 {
     public int Id { get; set; }
     public required decimal Amount { get; set; }
-    public required int CountInstallment{ get; set; }
-    public required int InterestRateAnnually { get; set; }
+    public required int InstallmentCount{ get; set; }
+    public required int AnnualInterestRate { get; set; }
 }
