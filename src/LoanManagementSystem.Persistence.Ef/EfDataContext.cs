dcc41e39d7d0f8f8c84510b50ec58c2a2f97b0b4
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-02 22:57:50 +0330

    setup DbContext

--- src/LoanManagementSystem.Persistence.Ef/EfDataContext.cs
+++ src/LoanManagementSystem.Persistence.Ef/EfDataContext.cs
@@ -1,6 +1,36 @@
+using LoanManagementSystem.Entities.Customers;
+using LoanManagementSystem.Entities.Installments;
+using LoanManagementSystem.Entities.LoanRequests;
+using LoanManagementSystem.Entities.Loans;
+using Microsoft.EntityFrameworkCore;
+
 namespace LoanManagementSystem.Persistence.Ef;
 
-public class EfDataContext
+public class EfDataContext : DbContext
 {
     
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<LoanRequest> LoanRequests { get; set; }
+    public DbSet<Installment> Installments { get; set; }
+    
+    public EfDataContext(
+        string connectionString)
+        : this(new DbContextOptionsBuilder<EfDataContext>()
+            .UseSqlServer(connectionString).Options)
+    {
+    }
+
+    public EfDataContext(
+        DbContextOptions<EfDataContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(Customer)
+            .Assembly);
+    }
 }
