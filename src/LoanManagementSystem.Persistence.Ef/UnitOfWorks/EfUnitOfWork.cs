358f24e761283edba4767265941b1ad3fa53377e
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-02 23:04:23 +0330

    setup UnitOfWork

--- src/LoanManagementSystem.Persistence.Ef/UnitOfWorks/EfUnitOfWork.cs
+++ src/LoanManagementSystem.Persistence.Ef/UnitOfWorks/EfUnitOfWork.cs
@@ -1,6 +1,18 @@
+using LoanManagementSystem.Services.UnitOfWorks;
+
 namespace LoanManagementSystem.Persistence.Ef.UnitOfWorks;
 
-public class EfUnitOfWork
+public class EfUnitOfWork(EfDataContext context) : UnitOfWork
 {
-    
+    public async Task Save() => await context.SaveChangesAsync();
+
+    public async Task Begin() => await context.Database.BeginTransactionAsync();
+
+    public async Task Rollback() => await context.Database.RollbackTransactionAsync();
+
+    public async Task Commit()
+    {
+        await context.SaveChangesAsync();
+        await context.Database.CommitTransactionAsync();
+    }
 }
