d3ef970234469f4581fc385e7c206d0f445f6d88
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-03 18:13:53 +0330

    setup charge method and implement it

--- src/LoanManagementSystem.Services/Customers/Contracts/Interfaces/CustomerService.cs
+++ src/LoanManagementSystem.Services/Customers/Contracts/Interfaces/CustomerService.cs
@@ -6,8 +6,10 @@ namespace LoanManagementSystem.Services.Customers.Contracts.Interfaces;
 public interface CustomerService
 {
     Task Register(AddCustomerDto dto);
+    Task RegisterWithStatement(AddCustomerWithStatementDto dto);
     Task Verify(int id);
     Task VerifyManually(int id);
-    Task Update(UpdateCustomerDto dto);
+    Task Charge(int id, UpdateBalanceDto dto);
+    Task Update(int id, UpdateCustomerDto dto);
     Task Delete(int id);
 }
--- src/LoanManagementSystem.Services/Customers/CustomerAppService.cs
+++ src/LoanManagementSystem.Services/Customers/CustomerAppService.cs
@@ -1,6 +1,146 @@
+using System;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using LoanManagementSystem.Entities.Customers;
+using LoanManagementSystem.Entities.Statements;
+using LoanManagementSystem.Entities.Statements.Enums;
+using LoanManagementSystem.Services.Customers.Contracts.DTOs;
+using LoanManagementSystem.Services.Customers.Contracts.Interfaces;
+using LoanManagementSystem.Services.Customers.Exceptions;
+using LoanManagementSystem.Services.UnitOfWorks;
+
 namespace LoanManagementSystem.Services.Customers;
 
-public class CustomerAppService
+public class CustomerAppService(
+    CustomerRepository customerRepository,
+    UnitOfWork unitOfWork
+) : CustomerService
 {
-    
+    public async Task Register(AddCustomerDto dto)
+    {
+        if (IsEmailValid(dto.Email))
+        {
+            throw new EmailNotValidException();
+        }
+
+        if (IsNationalIdValid(dto.NationalId))
+        {
+            throw new NationalIdDuplicateException();
+        }
+
+        if (await customerRepository.IsDuplicateByNationalId(dto.NationalId))
+        {
+            throw new NationalIdDuplicateException();
+        }
+
+        var customer = new Customer
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            NationalId = dto.NationalId,
+            PhoneNumber = dto.PhoneNumber,
+            Email = dto.Email,
+        };
+        await customerRepository.Add(customer);
+        await unitOfWork.Save();
+    }
+
+    public async Task RegisterWithStatement(AddCustomerWithStatementDto dto)
+    {
+        if (IsEmailValid(dto.Email))
+        {
+            throw new EmailNotValidException();
+        }
+
+        if (IsNationalIdValid(dto.NationalId))
+        {
+            throw new NationalIdDuplicateException();
+        }
+
+        if (await customerRepository.IsDuplicateByNationalId(dto.NationalId))
+        {
+            throw new NationalIdDuplicateException();
+        }
+
+        var incomeGroup = dto.Income > 10M ? IncomeGroup.MoreThanTen :
+            dto.Income >= 5M ? IncomeGroup.FiveUptoIncludingTen : IncomeGroup.LessThanFive;
+
+        var jobType = Enum.Parse(typeof(JobType), dto.JobType, true);
+
+        var customer = new Customer
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            NationalId = dto.NationalId,
+            PhoneNumber = dto.PhoneNumber,
+            Email = dto.Email,
+            Statement = new Statement
+            {
+                JobType = (JobType)jobType,
+                IncomeGroup = incomeGroup,
+                NetWorth = dto.NetWorth,
+            }
+        };
+        await customerRepository.Add(customer);
+        await unitOfWork.Save();
+    }
+
+    public async Task Verify(int id)
+    {
+        await Task.CompletedTask;
+        throw new NotImplementedException();
+    }
+
+    public async Task VerifyManually(int id)
+    {
+        var customer = await customerRepository.Find(id);
+        if (customer is null)
+        {
+            throw new CustomerNotFoundException();
+        }
+
+        customer.IsVerified = true;
+        await customerRepository.Update(customer);
+    }
+
+    public Task Charge(int id, UpdateBalanceDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Update(int id, UpdateCustomerDto dto)
+    {
+        var customer = await customerRepository.Find(id);
+        if (customer is null)
+        {
+            throw new CustomerNotFoundException();
+        }
+
+        if (dto.NationalId != null)
+        {
+            if (await customerRepository.IsDuplicateByNationalId(dto.NationalId))
+            {
+                throw new NationalIdDuplicateException();
+            }
+
+            customer.NationalId = dto.NationalId;
+        }
+
+        customer.FirstName = dto.FirstName ?? customer.FirstName;
+        customer.LastName = dto.LastName ?? customer.LastName;
+        customer.PhoneNumber = dto.PhoneNumber ?? customer.PhoneNumber;
+        customer.Email = dto.Email ?? customer.Email;
+    }
+
+    public Task Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static bool IsEmailValid(string email) =>
+        new MailAddress(email).Address != email;
+
+    private static bool IsNationalIdValid(string nationalId) =>
+        new GeneratedRegexAttribute("\\d{10}").Match(nationalId);
 }
