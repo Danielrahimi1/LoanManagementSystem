b5561a4ca5e4672149874fc273fe0fd5cb5a61ed
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-03 15:22:48 +0330

    add default values

--- src/LoanManagementSystem.Entities/Customers/Customer.cs
+++ src/LoanManagementSystem.Entities/Customers/Customer.cs
@@ -12,9 +12,9 @@ public class Customer
     public required string NationalId { get; set; }
     public required string PhoneNumber { get; set; }
     public required string Email { get; set; }
-    public decimal Balance { get; set; }
-    public bool IsVerified { get; set; }
-    public int CreditScore { get; set; }
+    public decimal Balance { get; set; } = default;
+    public bool IsVerified { get; set; } = default;
+    public int CreditScore { get; set; } = default;
     public Statement? Statement { get; set; }
     public HashSet<LoanRequest> LoanRequests { get; init; } = [];
 }
