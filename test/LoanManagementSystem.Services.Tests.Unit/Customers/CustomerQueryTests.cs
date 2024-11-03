2323a14021db82666a23e1ac8c60cbcc17845be9
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-03 15:24:50 +0330

    setup unit tests project with customers structure

--- test/LoanManagementSystem.Services.Tests.Unit/Customers/CustomerQueryTests.cs
+++ test/LoanManagementSystem.Services.Tests.Unit/Customers/CustomerQueryTests.cs
@@ -1,6 +1,13 @@
+using LoanManagementSystem.Services.Customers.Contracts.Interfaces;
+
 namespace LoanManagementSystem.Services.Tests.Unit.Customers;
 
 public class CustomerQueryTests
 {
+    private readonly CustomerQuery _sut;
     
+    public CustomerQueryTests()
+    {
+        // _sut = new EfCustomerQuery(ReadContext);
+    }
 }
--- test/LoanManagementSystem.Services.Tests.Unit/Customers/CustomerServiceTests.cs
+++ test/LoanManagementSystem.Services.Tests.Unit/Customers/CustomerServiceTests.cs
@@ -1,6 +1,13 @@
+using LoanManagementSystem.Services.Customers.Contracts.Interfaces;
+
 namespace LoanManagementSystem.Services.Tests.Unit.Customers;
 
 public class CustomerServiceTests
 {
-    
+    private readonly CustomerService _sut;
+
+    public CustomerServiceTests()
+    {
+        // _sut = CustomerServiceFactory.CreateService(SetupContext);
+    }
 }
