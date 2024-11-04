a9a8b7f4ea60f893efb335e3aeacf84ab78d5529
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-04 16:33:09 +0330

    implement Loan Query and Repository

--- src/LoanManagementSystem.Persistence.Ef/Loans/EfLoanQuery.cs
+++ src/LoanManagementSystem.Persistence.Ef/Loans/EfLoanQuery.cs
@@ -1,6 +1,48 @@
+using LoanManagementSystem.Entities.Loans;
+using LoanManagementSystem.Services.Loans.Contracts.DTOs;
+using LoanManagementSystem.Services.Loans.Contracts.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace LoanManagementSystem.Persistence.Ef.Loans;
 
-public class EfLoanQuery
+public class EfLoanQuery(EfDataContext context) : LoanQuery
 {
+    public async Task<GetLoanDto?> GetById(int id) =>
+        await (from loan in context.Set<Loan>()
+            where loan.Id == id
+            select new GetLoanDto
+            {
+                Amount = loan.Amount,
+                InstallmentCount = loan.InstallmentCount,
+                AnnualInterestRate = loan.AnnualInterestRate,
+            }).FirstOrDefaultAsync();
+
+    public async Task<GetLoanDto[]> GetAll() =>
+        await (from loan in context.Set<Loan>()
+            select new GetLoanDto
+            {
+                Amount = loan.Amount,
+                InstallmentCount = loan.InstallmentCount,
+                AnnualInterestRate = loan.AnnualInterestRate,
+            }).ToArrayAsync();
     
+    public async Task<GetLoanDto[]> GetAllShortPeriod() =>
+        await (from loan in context.Set<Loan>()
+            where loan.InstallmentCount <= 12
+            select new GetLoanDto
+            {
+                Amount = loan.Amount,
+                InstallmentCount = loan.InstallmentCount,
+                AnnualInterestRate = loan.AnnualInterestRate,
+            }).ToArrayAsync();
+
+    public async Task<GetLoanDto[]> GetAllLongPeriod() =>
+        await (from loan in context.Set<Loan>()
+            where loan.InstallmentCount > 12
+            select new GetLoanDto
+            {
+                Amount = loan.Amount,
+                InstallmentCount = loan.InstallmentCount,
+                AnnualInterestRate = loan.AnnualInterestRate,
+            }).ToArrayAsync();
 }
--- src/LoanManagementSystem.Persistence.Ef/Loans/EfLoanRepository.cs
+++ src/LoanManagementSystem.Persistence.Ef/Loans/EfLoanRepository.cs
@@ -6,14 +6,15 @@ namespace LoanManagementSystem.Persistence.Ef.Loans;
 
 public class EfLoanRepository(EfDataContext context) : LoanRepository
 {
-    public async Task Add(Loan loan) => await context.Set<Loan>().AddAsync(loan);
+    public async Task Add(Loan loan) =>
+        await context.Set<Loan>().AddAsync(loan);
 
     public async Task<Loan?> Find(int id) =>
         await context.Set<Loan>().FirstOrDefaultAsync(loan => loan.Id == id);
 
-    public async Task Update(Loan loan) =>
-        await Task.Run(() => context.Set<Loan>().Update(loan));
+    public void Update(Loan loan) =>
+        context.Set<Loan>().Update(loan);
 
-    public async Task Remove(Loan loan) =>
-        await Task.Run(() => context.Set<Loan>().Remove(loan));
+    public void Remove(Loan loan) =>
+        context.Set<Loan>().Remove(loan);
 }
