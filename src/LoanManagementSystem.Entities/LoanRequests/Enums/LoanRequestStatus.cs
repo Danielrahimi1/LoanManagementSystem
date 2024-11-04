d4aa78581fa9aabe8d54601c196cde6fdf7a9db5
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-04 23:14:24 +0330

    add LoanRequest enums

---
+++ src/LoanManagementSystem.Entities/LoanRequests/Enums/LoanRequestStatus.cs
@@ -0,0 +1,10 @@
+namespace LoanManagementSystem.Entities.LoanRequests.Enums;
+
+public enum LoanRequestStatus
+{
+    Review = 1,
+    Accept = 2,
+    Reject = 3,
+    Active = 4,
+    Close = 5
+}
