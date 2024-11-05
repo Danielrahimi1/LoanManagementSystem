615483bf2b200470bbaaa039e9e80e472d27f567
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-05 14:14:47 +0330

    make LoanRequestId unwanted

--- src/LoanManagementSystem.Entities/Installments/Installment.cs
+++ src/LoanManagementSystem.Entities/Installments/Installment.cs
@@ -6,8 +6,8 @@ namespace LoanManagementSystem.Entities.Installments;
 public class Installment
 {
     public int Id { get; init; }
+    public int LoanRequestId { get; init; }
     public required LoanRequest LoanRequest { get; init; }
-    public required int LoanRequestId { get; init; }
     public required decimal Amount { get; init; }
     public required decimal MonthlyInterest { get; init; }
     public required DateOnly PaymentDeadLine { get; init; }
