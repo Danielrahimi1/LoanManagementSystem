fc7b1ef13a2bdd32de86328eab607c55a42d5fa3
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-05 17:43:04 +0330

    delete Statement entity

--- src/LoanManagementSystem.Services/Statements/Contracts/DTOs/UpdateStatementDto.cs
+++
@@ -1,9 +0,0 @@
-namespace LoanManagementSystem.Services.Statements.Contracts.DTOs;
-
-public class UpdateStatementDto
-{
-    public int CustomerId { get; init; }
-    public string JobType { get; set; } = "UnEmployed";
-    public decimal Income { get; set; } = default;
-    public decimal NetWorth { get; set; }
-}
