41da53bc3158abd07b0a1ce3f29efebb8a1ad146
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-05 18:26:11 +0330

    implement CustomerQuery

--- src/LoanManagementSystem.Persistence.Ef/Customers/EfCustomerQuery.cs
+++ src/LoanManagementSystem.Persistence.Ef/Customers/EfCustomerQuery.cs
@@ -1,6 +1,116 @@
+using LoanManagementSystem.Entities.Customers;
+using LoanManagementSystem.Entities.Installments;
+using LoanManagementSystem.Entities.LoanRequests;
+using LoanManagementSystem.Services.Customers.Contracts.DTOs;
+using LoanManagementSystem.Services.Customers.Contracts.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace LoanManagementSystem.Persistence.Ef.Customers;
 
-public class EfCustomerQuery
+public class EfCustomerQuery(EfDataContext context) : CustomerQuery
 {
-    
+    public async Task<GetCustomerDto?> GetById(int id) =>
+        await (from c in context.Set<Customer>()
+            where c.Id == id
+            select new GetCustomerDto
+            {
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                NationalId = c.NationalId,
+                PhoneNumber = c.PhoneNumber,
+                Email = c.Email,
+                Balance = c.Balance,
+                IsVerified = c.IsVerified,
+                CreditScore = c.CreditScore
+            }).FirstOrDefaultAsync();
+
+    public async Task<GetCustomerWithStatementDto?> GetByIdWithStatement(int id) =>
+        await (from c in context.Set<Customer>()
+            where c.Id == id
+            select new GetCustomerWithStatementDto
+            {
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                NationalId = c.NationalId,
+                PhoneNumber = c.PhoneNumber,
+                Email = c.Email,
+                Balance = c.Balance,
+                IsVerified = c.IsVerified,
+                CreditScore = c.CreditScore,
+                JobType = c.JobType.ToString(),
+                IncomeGroup = c.JobType.ToString(),
+                NetWorth = c.NetWorth
+            }).FirstOrDefaultAsync();
+
+    public async Task<GetCustomerDto[]> GetAll() =>
+        await (from c in context.Set<Customer>()
+            select new GetCustomerDto
+            {
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                NationalId = c.NationalId,
+                PhoneNumber = c.PhoneNumber,
+                Email = c.Email,
+                Balance = c.Balance,
+                IsVerified = c.IsVerified,
+                CreditScore = c.CreditScore
+            }).ToArrayAsync();
+
+    public async Task<GetCustomerWithStatementDto[]> GetAllWithStatement() =>
+        await (from c in context.Set<Customer>()
+            select new GetCustomerWithStatementDto
+            {
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                NationalId = c.NationalId,
+                PhoneNumber = c.PhoneNumber,
+                Email = c.Email,
+                Balance = c.Balance,
+                IsVerified = c.IsVerified,
+                CreditScore = c.CreditScore,
+                JobType = c.JobType.ToString(),
+                IncomeGroup = c.JobType.ToString(),
+                NetWorth = c.NetWorth
+            }).ToArrayAsync();
+
+    public async Task<GetCustomerDto[]> GetRiskyCustomers() =>
+        await (from i in context.Set<Installment>()
+            where i.Fine > 0
+            join lr in context.Set<LoanRequest>()
+                on i.LoanRequestId equals lr.Id
+            join c in context.Set<Customer>()
+                on lr.CustomerId equals c.Id
+            select new GetCustomerDto
+            {
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                NationalId = c.NationalId,
+                PhoneNumber = c.PhoneNumber,
+                Email = c.Email,
+                Balance = c.Balance,
+                IsVerified = c.IsVerified,
+                CreditScore = c.CreditScore,
+            }).ToArrayAsync();
+
+    public async Task<GetCustomerWithStatementDto[]> GetRiskyCustomersWithStatement() =>
+        await (from i in context.Set<Installment>()
+            where i.Fine > 0
+            join lr in context.Set<LoanRequest>()
+                on i.LoanRequestId equals lr.Id
+            join c in context.Set<Customer>()
+                on lr.CustomerId equals c.Id
+            select new GetCustomerWithStatementDto
+            {
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                NationalId = c.NationalId,
+                PhoneNumber = c.PhoneNumber,
+                Email = c.Email,
+                Balance = c.Balance,
+                IsVerified = c.IsVerified,
+                CreditScore = c.CreditScore,
+                JobType = c.JobType.ToString(),
+                IncomeGroup = c.JobType.ToString(),
+                NetWorth = c.NetWorth
+            }).ToArrayAsync();
 }
