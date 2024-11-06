33e618425d2b6ef691015512c844b25daeb9df29
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-06 21:42:04 +0330

    setup LoanRequestServiceFactory

---
+++ test/LoanManagementSystem.TestTools/LoanRequests/LoanRequestServiceFactory.cs
@@ -0,0 +1,34 @@
+using LoanManagementSystem.Persistence.Ef;
+using LoanManagementSystem.Persistence.Ef.Customers;
+using LoanManagementSystem.Persistence.Ef.Installments;
+using LoanManagementSystem.Persistence.Ef.LoanRequests;
+using LoanManagementSystem.Persistence.Ef.Loans;
+using LoanManagementSystem.Persistence.Ef.UnitOfWorks;
+using LoanManagementSystem.Services;
+using LoanManagementSystem.Services.LoanRequests;
+using LoanManagementSystem.Services.LoanRequests.Contracts.Interfaces;
+using Moq;
+
+namespace LoanManagementSystem.TestTools.LoanRequests;
+
+public static class LoanRequestServiceFactory
+{
+    public static LoanRequestService CreateService(EfDataContext context)
+    {
+        var loanRequestRepository = new EfLoanRequestRepository(context);
+        var customerRepository = new EfCustomerRepository(context);
+        var loanRepository = new EfLoanRepository(context);
+        var installmentRepository = new EfInstallmentRepository(context);
+        var dateService = new Mock<DateService>().Object; 
+        var unitOfWork = new EfUnitOfWork(context);
+        
+        return new LoanRequestAppService(
+            loanRequestRepository,
+            customerRepository,
+            loanRepository,
+            installmentRepository,
+            dateService,
+            unitOfWork
+        );
+    }
+}
--- test/LoanManagementSystem.TestTools/Loans/LoanQueryFactory.cs
+++
@@ -1,6 +0,0 @@
-namespace LoanManagementSystem.TestTools.Loans;
-
-public class LoanQueryFactory
-{
-    
-}
