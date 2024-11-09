d1cd43816a0c1bf08b04acd552b3eeff7b27357d
Danial Rahimi <danielrahimi1@gmail.com>
2024-11-09 11:41:33 +0330

    setup installment service test

--- test/LoanManagementSystem.Services.Tests.Unit/Installments/InstallmentServiceTests.cs
+++ test/LoanManagementSystem.Services.Tests.Unit/Installments/InstallmentServiceTests.cs
@@ -1,6 +1,13 @@
+using LoanManagementSystem.Services.Installments.Contracts.Interfaces;
+using LoanManagementSystem.TestTools.Infrastructure.DataBaseConfig.Integration;
+using LoanManagementSystem.TestTools.Installments;
+
 namespace LoanManagementSystem.Services.Tests.Unit.Installments;
 
-public class InstallmentServiceTests
+public class InstallmentServiceTests : BusinessIntegrationTest
 {
-    
+    private readonly InstallmentService _sut;
+
+    public InstallmentServiceTests() =>
+        _sut = InstallmentServiceFactory.CreateService(SetupContext);
 }
---
+++ test/LoanManagementSystem.TestTools/Installments/InstallmentServiceFactory.cs
@@ -0,0 +1,17 @@
+using LoanManagementSystem.Persistence.Ef;
+using LoanManagementSystem.Persistence.Ef.Installments;
+using LoanManagementSystem.Persistence.Ef.UnitOfWorks;
+using LoanManagementSystem.Services.Installments;
+using LoanManagementSystem.Services.Installments.Contracts.Interfaces;
+
+namespace LoanManagementSystem.TestTools.Installments;
+
+public static class InstallmentServiceFactory
+{
+    public static InstallmentService CreateService(EfDataContext context)
+    {
+        var installmentRepository = new EfInstallmentRepository(context);
+        var unitOfWork = new EfUnitOfWork(context);
+        return new InstallmentAppService(installmentRepository, unitOfWork);
+    }
+}
